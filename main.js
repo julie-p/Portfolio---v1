e0aaf77c09066fc07744f81c3c1492e63ad111f4
Julie P <paupertjulie@gmail.com>
2020-05-22 19:29:51 +0200

    D.O.N.E!

--- main.js
+++ main.js
@@ -15,4 +15,17 @@ $(document).ready(function () {
             scrollTop: $($(this).attr('href')).offset().top - 100
         }, 2000);
     });
+
+    $('#up').on('click', function () {
+        $('html, body').animate( {
+            scrollTop: 0
+        }, 2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
+
 });
