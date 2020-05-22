bf470f49a90c7954181f4e9d721a7c1311b3660e
Julie P <paupertjulie@gmail.com>
2020-05-22 17:04:16 +0200

    styling done - jquery & animations next

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
+    //Ouvre le menu
     $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
-    })
+    });
+    //Ferme le menu s/click et redirige vers la section
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+    //Smooth scroll
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate( {
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+    });
 });
